66655045119e2d97860ca8f92867e41427e076b4
Hrithik kanoje <128607033+OrbotOp@users.noreply.github.com>
2023-06-05 19:57:55 -0400

    day-1

---
+++ scripts/image_show.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import  cv2
+cap = cv2.VideoCapture(0)
+print(cap.isOpened())
+
+if not cap.isOpened():
+    print("can not open camera")
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
